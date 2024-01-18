274f2f35ec8bf65887c51fffa1047043fdd95cee
Jeremy Liang <59520795+Averesoft@users.noreply.github.com>
2024-01-18 12:08:44 -0500

    Update simple.c

--- simple.c
+++ simple.c
@@ -15,3 +15,23 @@ static void init()
     initHeap(heap, heap + sizeof(heap));
     init_run = true;
 }
+
+static Logic respawnLogic = 
+{
+    {&actionStateEq, .arg1.u = 2, .arg2.u = _AS_RebirthWait},
+    {&addMove, .arg1.p = &cpuPlayer, .arg2.p = &_mv_shortHop}
+};
+
+static void loadDefaultLogic() {
+    addLogic(&cpuPlayer, &respawnLogic);
+
+}
+
+
+void _main() {
+    if (!init_run) { init(); }
+
+    if (needLogic(&cpuPlayer)) { loadDefaultLogic(); }
+
+    updateAI(&cpuPlayer);
+}
